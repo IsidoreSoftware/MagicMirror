dc6a070c7781a93b9a976796907b74fb7ecd6ee0
Kuba Matjanowski <k.matjanowski@gmail.com>
2017-02-19 21:24:48 +0100

    optimized file stream reading

--- src/Isidore.MagicMirror.Utils/Helpers/IO/StreamHelper.cs
+++ src/Isidore.MagicMirror.Utils/Helpers/IO/StreamHelper.cs
@@ -1,24 +1,63 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 
 namespace Isidore.MagicMirror.Utils.Helpers.IO
 {
     public static class StreamHelper
     {
+        public  const int ByteCount = 4096;
+
         public static async Task<byte[]> ToByteArray(this Stream stream)
         {
-            byte[] bytes = new byte[stream.Length + 10];
-            int numBytesToRead = (int)stream.Length;
-            int numBytesRead = 0;
-            do
-            {
-                // Read may return anything from 0 to 10.
-                int n = await stream.ReadAsync(bytes, numBytesRead, 10);
-                numBytesRead += n;
-                numBytesToRead -= n;
-            } while (numBytesToRead > 0);
+            long originalPosition = 0;
 
-            return bytes;
+            if (stream.CanSeek)
+            {
+                originalPosition = stream.Position;
+                stream.Position = 0;
+            }
+
+            try
+            {
+                byte[] readBuffer = new byte[ByteCount];
+
+                int totalBytesRead = 0;
+                int bytesRead;
+
+                while ((bytesRead = await stream.ReadAsync(readBuffer, totalBytesRead, readBuffer.Length - totalBytesRead)) > 0)
+                {
+                    totalBytesRead += bytesRead;
+
+                    if (totalBytesRead == readBuffer.Length)
+                    {
+                        int nextByte = stream.ReadByte();
+                        if (nextByte != -1)
+                        {
+                            byte[] temp = new byte[readBuffer.Length * 2];
+                            Buffer.BlockCopy(readBuffer, 0, temp, 0, readBuffer.Length);
+                            Buffer.SetByte(temp, totalBytesRead, (byte)nextByte);
+                            readBuffer = temp;
+                            totalBytesRead++;
+                        }
+                    }
+                }
+
+                byte[] buffer = readBuffer;
+                if (readBuffer.Length != totalBytesRead)
+                {
+                    buffer = new byte[totalBytesRead];
+                    Buffer.BlockCopy(readBuffer, 0, buffer, 0, totalBytesRead);
+                }
+                return buffer;
+            }
+            finally
+            {
+                if (stream.CanSeek)
+                {
+                    stream.Position = originalPosition;
+                }
+            }
         }
     }
 }
