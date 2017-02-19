3e5471af3e16d66cdea10a73e3233bbdd97aefd3
Kuba Matjanowski <k.matjanowski@gmail.com>
2017-02-19 21:33:18 +0100

    clean up

--- src/Isidore.MagicMirror.Utils/Helpers/IO/StreamHelper.cs
+++ src/Isidore.MagicMirror.Utils/Helpers/IO/StreamHelper.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.IO;
+﻿using System.IO;
 using System.Threading.Tasks;
 
 namespace Isidore.MagicMirror.Utils.Helpers.IO
 {
     public static class StreamHelper
     {
-        public  const int ByteCount = 4096;
-
         public static async Task<byte[]> ToByteArray(this Stream stream)
         {
             using (var memoryStream = new MemoryStream())
