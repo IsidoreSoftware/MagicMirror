85ee4e3285c507246137294dccbc9e11b89be6bf
Kuba Matjanowski <k.matjanowski@gmail.com>
2017-02-24 00:30:25 +0100

    Added test project and empty service

---
+++ src/Isidore.MagicMirror.Users/Services/UserService.cs
@@ -0,0 +1,6 @@
+﻿namespace Isidore.MagicMirror.Users.Services
+{
+    public class UserService
+    {
+    }
+}
---
+++ tests/Isidore.MagicMirror.Users.Tests/UserServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using Isidore.MagicMirror.Users.Services;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Isidore.MagicMirror.Users.Tests
+{
+    public class UserServiceTests
+    {
+        public UserServiceTests()
+        {
+        }
+
+        [Fact]
+        public async Task can_create_user_service()
+        {
+            var userService = new UserService();
+        }
+    }
+}
