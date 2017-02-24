81e5bf7f4f3bf209088753ad21f5c6b57cb8723a
Kuba Matjanowski <k.matjanowski@gmail.com>
2017-02-24 23:44:31 +0100

    Moving to VS2017

---
+++ Isidore.MagicMirror.API.Tests/FaceTests.cs
@@ -0,0 +1,14 @@
+﻿using Isidore.MagicMirror.API.Controllers;
+using Xunit;
+
+namespace Isidore.MagicMirror.API.Tests
+{
+    public class FaceTests
+    {
+        [Fact]
+        public void can_create_face_controller()
+        {
+            var a = new FacesController();
+        }
+    }
+}
