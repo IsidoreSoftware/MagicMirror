d4b54523b8932a49ac737b2f7c3611786dd014c9
Kuba Matjanowski <k.matjanowski@gmail.com>
2017-08-10 01:24:06 +0200

    added Azure client

--- src/Isidore.MagicMirror.Users/Isidore.MagicMirror.ImageProcessing/FaceRecognition/Services/AzureFaceRecognitionService.cs
+++ src/Isidore.MagicMirror.Users/Isidore.MagicMirror.ImageProcessing/FaceRecognition/Services/AzureFaceRecognitionService.cs
@@ -1,14 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Threading.Tasks;
 using Isidore.MagicMirror.ImageProcessing.FaceRecognition.Models;
 using Isidore.MagicMirror.Users.Models;
+using Microsoft.ProjectOxford.Face;
 
 namespace Isidore.MagicMirror.ImageProcessing.FaceRecognition.Services
 {
     public class AzureFaceRecognitionService : IFaceRecognitionService<byte[],User>
     {
+        private readonly IFaceServiceClient _faceServiceClient;
+
+        public AzureFaceRecognitionService(IFaceServiceClient faceServiceClient)
+        {
+            _faceServiceClient = faceServiceClient;
+        }
+
         public Task<RecognitionResult<User>> RecognizeAsync(byte[] image)
         {
             throw new NotImplementedException();
